025617e91da301d2284173ee6a5019be64568b2c
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-08-09 00:23:19 +0100

    Updated js and button tag

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   let squares = Array.from(document.querySelectorAll('.grid div'))
   const scoreDisplay = document.querySelector('#score')
-  const startBtn = document.querySelector('#start-button')
+  const startBtn = document.querySelector('#button')
   const width = 10
   let nextRandom = 0
   let timerId // this is null
